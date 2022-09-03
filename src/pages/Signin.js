75efc8752de0ccea43e0d0f3fff22c3c0bd269bc
ultimecreation <naserbechata@gmail.com>
2022-09-03 16:46:45 +0200

    added auth login/logout routine

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Navigate } from 'react-router-dom';
+import { login } from "../redux/auth/AuthActions";
 
 export class Signin extends Component {
     constructor(props) {
         super(props)
+        
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            errors: [],
+            rememberMe: false
         }
+        this.errors = []
+        
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
     }
@@ -18,22 +26,68 @@ export class Signin extends Component {
                 [e.target.name]: e.target.value
             }
         })
-
+        if (e.target.name === "rememberMe") {
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    [e.target.name]: e.target.checked
+                }
+            })
+        }
     }
 
-    handleSubmit(e) {
+    async handleSubmit(e) {
         e.preventDefault()
+        this.errors = []
+        
+        // check for errors
+        if (this.state.username === '') this.errors.push({ errorText: "L'email est requis" })
+        if (this.state.password === '') this.errors.push({ errorText: "Le mot de passe est requis" })
+        this.checkForErrors()
 
+        const user = {
+            email: this.state.username,
+            password: this.state.password
+        }
+
+        await this.props.login(user)
+    }
+    
+    checkForErrors() {
+        if (this.errors.length > 0) {
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    errors: [...this.errors]
+                }
+            })
+            return
+        }
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                errors: []
+            }
+        })
     }
 
     render() {
-        console.log(this.state)
+        if (this.props.isAuthenticated === true) return <Navigate replace to="/profile" />
         return (
             <main className="main bg-dark">
                 <section className="sign-in-content">
                     <i className="fa fa-user-circle sign-in-icon"></i>
                     <h1>Sign In</h1>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
+                        <div id="sign-in-error-container">
+                            {
+                                this.state.errors.length > 0
+                                && this.state.errors.map((error, index) => {
+                                    return <p key={index}>{error.errorText}</p>
+                                })
+                            }
+                        </div>
+
                         <div className="input-wrapper">
                             <label htmlFor="username">Username</label>
                             <input type="text" id="username" name="username" onChange={this.handleChange} />
@@ -43,7 +97,7 @@ export class Signin extends Component {
                             <input type="password" id="password" name="password" onChange={this.handleChange} autoComplete="off" />
                         </div>
                         <div className="input-remember">
-                            <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
+                            <input type="checkbox" id="remember-me" name="rememberMe" onChange={this.handleChange} /><label htmlFor="remember-me">Remember me</label>
                         </div>
 
                         {/* <a href="./user.html" className="sign-in-button">Sign In</a> */}
@@ -57,4 +111,10 @@ export class Signin extends Component {
     }
 }
 
-export default Signin
+const mapStateToProps = state => {
+    return {
+        isLoading: state.AuthReducer.isLoading,
+        isAuthenticated: state.AuthReducer.isAuthenticated
+    }
+}
+export default connect(mapStateToProps, { login })(Signin) 
--- src/redux/auth/AuthActions.js
+++ src/redux/auth/AuthActions.js
@@ -1,18 +1,23 @@
-import { AUTH_LOGIN_ATTEMPT, AUTH_LOGIN_FAILURE, AUTH_LOGIN_SUCCESS } from "./AuthTypes"
+import { Navigate } from "react-router-dom"
+import { AUTH_LOGIN_ATTEMPT, AUTH_LOGIN_FAILURE, AUTH_LOGIN_SUCCESS, AUTH_LOGOUT } from "./AuthTypes"
 
 export const login = incomingUserData => async dispatch =>{
 
     dispatch({type: AUTH_LOGIN_ATTEMPT})
 
-    const userFound = await fetch("http://localhost:3001/api/v1/user/login",{
+    const response = await fetch("http://localhost:3001/api/v1/user/login",{
         headers: {
             "Content-Type":"application/json"
         },
         method: "POST",
         body: JSON.stringify(incomingUserData)
     })
+    const data = await response.json()
 
-    userFound 
-        ? dispatch({type: AUTH_LOGIN_SUCCESS, payload: userFound})
-        : dispatch({type: AUTH_LOGIN_FAILURE})
+    if(data.status === 400) return dispatch({type: AUTH_LOGIN_FAILURE})
+    if(data.status === 200) return dispatch({type: AUTH_LOGIN_SUCCESS, payload: data.body.token})
 }
+
+export const logout = () =>  dispatch =>{
+dispatch({type: AUTH_LOGOUT})
+} 
